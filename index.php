746214fb3c738078b318febe9e37a5e7ce36927d
Rizki Dian Pratama <rizkidianpratama784@gmail.com>
2023-10-02 19:46:46 +0700

    [FIX] Perbaikan bug

--- index.php
+++ index.php
@@ -4,58 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Login</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="assets/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
-<body class="login">
-    <div class="container d-flex justify-content-center align-items-center min-vh-100">
-       <div class="row border rounded-5 p-3 bg-white shadow box-area">
-       <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" style="background: #41a4bf;">
-           <div class="featured-image mb-0">
-            <img src="assets/Images/login-removebg-preview.png" class="img-fluid" style="width: 300px;">
-           </div>
-           <p class="text-white fs-3" style="font-family: 'Courier New', Courier, monospace; font-weight: 600;">Sign In Your Acount</p>
-           <small class="text-white text-wrap text-center" style="width: 17rem;font-family: 'Courier New', Courier, monospace;">Join experienced Designers on this platform.</small>
-       </div> 
-        
-       <div class="col-md-6 right-box">
-          <div class="row align-items-center">
-                <div class="header-text mb-4">
-                     <h2>Hello,Again</h2>
-                     <p>We are happy to have you back.</p>
-                </div>
-                <form action="proses_login.php" method="post">
-                <div class="input-group mb-3">
-                    <input type="text" id="username" name="username" class="form-control form-control-lg bg-light fs-6" placeholder="Username">
-                </div>
-                <div class="input-group mb-1">
-                    <input type="password" id="password" name="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password">
-                </div>
-                <div class="input-group mb-5 d-flex justify-content-between">
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="formCheck">
-                        <label for="formCheck" class="form-check-label text-secondary"><small>Remember Me</small></label>
-                    </div>
-                    <div class="forgot">
-                        <small><a href="#">Forgot Password?</a></small>
-                    </div>
-                </div>
-                <div class="input-group mb-3">
-                    <button class="btn btn-lg btn-info w-100 fs-6">Login</button>
-                </div>
-                </form>
-                <div class="input-group mb-3">
-                    <button class="btn btn-lg btn-light w-100 fs-6"><img src="assets/Images/google.png" style="width:20px" class="me-2"><small>Sign In with Google</small></button>
-                </div>
-                <div class="row">
-                    <small>Don't have account? <a href="#">Sign Up</a></small>
-                </div>
-          </div>
-       </div> 
-      </div>
-    </div>
-    <!-- /.login-card-body -->
+<body>
+    <?php 
+    include ('login.php')
+    ?>
 </body>
 </html>
--- login.php
+++ login.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Login</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
