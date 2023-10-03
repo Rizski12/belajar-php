ab9cb8b860e5aab88710448dd543a16e529d9400
Rizki Dian Pratama <rizkidianpratama784@gmail.com>
2023-10-03 17:41:10 +0700

    [FEAT] Variabel produk

---
+++ pages/variabel-produk.php
@@ -0,0 +1,111 @@
+<?php
+$produk1 = array(
+    "nama" => "Laptop Asus",
+    "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "toko" => "@RizskiShop",
+    "harga" => 150,
+    "gambar" => "../assets/images/produk/asusku.jpg",
+    "badge" => "Sold Out",
+);
+
+$produk2 = array(
+    "nama" => "Jaket",
+    "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "toko" => "@RizskiShop",
+    "harga" => 150,
+    "gambar" => "../assets/images/produk/jaket.jpg",
+    "badge" => "HOT",
+);
+
+$produk3 = array(
+    "nama" => "Helm",
+    "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "toko" => "@RizskiShop",
+    "harga" => 100,
+    "gambar" => "../assets/images/kategori/helm.jpg",
+    "badge" => "HOT",
+);
+
+$produk4 = array(
+    "nama" => "Kipas Angin",
+    "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "toko" => "@RizskiShop",
+    "harga" => 150,
+    "gambar" => "../assets/images/produk/kipas.jpg",
+    "badge" => "HOT",
+);
+
+$produk5 = array(
+    "nama" => "Tas Eiger",
+    "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "toko" => "@RizskiShop",
+    "harga" => 150,
+    "gambar" => "../assets/images/produk/eiger.jpg",
+    "badge" => "Sold Out",
+);
+
+$produk6 = array(
+    "nama" => "Jam Tangan",
+    "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "toko" => "@RizskiShop",
+    "harga" => 150,
+    "gambar" => "../assets/images/produk/jam.jpg",
+    "badge" => "HOT",
+);
+
+$produk7 = array(
+    "nama" => "Pulpen",
+    "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "toko" => "@RizskiShop",
+    "harga" => 150,
+    "gambar" => "../assets/images/produk/pulpen.jpg",
+    "badge" => "HOT",
+);
+
+$produk8 = array(
+    "nama" => "Vivo",
+    "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "toko" => "@RizskiShop",
+    "harga" => 150,
+    "gambar" => "../assets/images/produk/vivo.jpg",
+    "badge" => "Sold Out",
+);
+
+$produk9 = array(
+    "nama" => "Blender",
+    "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "toko" => "@RizskiShop",
+    "harga" => 150,
+    "gambar" => "../assets/images/produk/blender.jpg",
+    "badge" => "Sold Out",
+);
+
+$produk10 = array(
+    "nama" => "Baju Cowo",
+    "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "toko" => "@RizskiShop",
+    "harga" => 150,
+    "gambar" => "../assets/images/produk/bajucowo.jpg",
+    "badge" => "HOT",
+);
+
+$produk11 = array(
+    "nama" => "Jam Tangan Wanita",
+    "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "toko" => "@RizskiShop",
+    "harga" => 150,
+    "gambar" => "../assets/images/produk/jamwanita.jpg",
+    "badge" => "HOT",
+);
+
+$produk12 = array(
+    "nama" => "Helm LTD Sport",
+    "deskripsi" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "toko" => "@RizskiShop",
+    "harga" => 150,
+    "gambar" => "../assets/images/produk/helmltd.jpg",
+    "badge" => "Sold Out",
+);
+
+$produkList = array($produk1, $produk2, $produk3, $produk4, $produk5, $produk6, $produk7, $produk8, $produk9, $produk10, $produk11, $produk12);
+?>
