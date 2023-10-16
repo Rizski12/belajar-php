ca69b5ceaf28912439d279bc1cd5d139779e9f89
Rizki Dian Pratama <rizkidianpratama784@gmail.com>
2023-10-16 11:29:41 +0700

    [FEAT] Partial untuk halaman dashboard

---
+++ pages/_partials/sidebar.php
@@ -0,0 +1,91 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="dashboard.html" class="brand-link">
+      <img src="../../assets/Images/arkatama.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">AdminLTE 3</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="../../assets/Images/rizki.jpg" class="img-circle elevation-3" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Rizki Dian Pratama</a>
+        </div>
+      </div>
+
+      <!-- SidebarSearch Form -->
+      <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="../dashboard/dashboard.php" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+                <i class="fas fa-angle-right right"></i>
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="../array-dash.php" class="nav-link">
+              <i class="nav-icon fas fa-shopping-cart"></i>
+              <p>
+                Array-Products
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="../../index.php" class="nav-link">
+              <i class="nav-icon far fa-user"></i>
+              <p>
+                Login
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="../listing-produk.php" class="nav-link">
+              <i class="nav-icon fas fa-th-list"></i>
+              <p>
+                List-Products
+              </p>
+            </a>
+          </li>
+          <li class="nav-item bg-secondary">
+            <a href="../crud/index.php" class="nav-link">
+              <i class="nav-icon fas fa-cube"></i>
+              <p>
+                CRUD-Products
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="../login/logout.php" class="nav-link">
+              <i class="nav-icon fas fa-sign-out-alt"></i>
+              <p>
+                LogOut
+              </p>
+            </a>
+          </li>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
